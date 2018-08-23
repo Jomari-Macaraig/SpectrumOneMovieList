d1423e5bbdb1d1be701910f68d81c625eb0d6b8b
Jomari Macaraig <macaraig.jomari10@gmail.com>
2018-08-23 15:05:54 +0000

    #1 Update settings files and files dependent on it

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'movielist.urls'
+ROOT_URLCONF = 'conf.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'movielist.wsgi.application'
+WSGI_APPLICATION = 'conf.wsgi.application'
 
 
 # Database
